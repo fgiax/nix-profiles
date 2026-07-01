3ad9090acdc39abeb242d328124121695ece5059
zuger <fgiax02@gmail.com>
2026-07-01 20:09:06 +0000

    profile: enable cross-platform builds via flake-utils

--- default/flake.nix
+++ default/flake.nix
@@ -1,31 +1,35 @@
 # use alejandra for modern style
 # Format with: `alejandra flake.nix`
 {
-  description = "Default profile (don't use home-manager)";
+  description = "Default CLI profile";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    system = "aarch64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system}.default = pkgs.buildEnv {
-      name = "default";
-      paths = with pkgs; [
-        zsh
-        fzf
-        file
-        python3
-        tmux
-        ranger
-        alejandra # formatter for nix
-        neovim
-      ];
-    };
-  };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        packages.default = pkgs.buildEnv {
+          name = "default-profile";
+          paths = with pkgs; [
+            zsh
+            fzf
+            file
+            python3
+            tmux
+            ranger
+            alejandra
+            neovim
+          ];
+        };
+      }
+    );
 }
